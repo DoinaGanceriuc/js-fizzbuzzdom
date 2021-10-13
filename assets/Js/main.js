7ce99dc9565dec3f47343f3715ef1f4fe2e83845
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-13 16:29:02 +0200

    add second condition
    aggiunta condizione multipli 3

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,7 +1,5 @@
 /* Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
-per i multipli di 3, 
 per i multipli di 5
- e per i valori che sono sia multipli di 3 che di 5.
  */
 
 // selezionare un elemento dal DOM
@@ -16,9 +14,15 @@ containerElement.insertAdjacentHTML("afterbegin", "<h1>FizzBuzzDOM</h1>");
 for (let i = 1; i <= 100; i++) {
   /* console.log(i); */
 
+  /* aggiungere uno stile differente a seconda del valore dell'indice per multipli di 3 e di 5 */
   if (i % 3 == 0 && i % 5 == 0) {
     const element = `<li class="box fizzbuzz - ${i}"> fizzbuzz </li>`;
     listElement.insertAdjacentHTML("beforeend", element);
     /* console.log(element); */
+
+    /* multipli di 3*/
+  } else if (i % 3 == 0) {
+    const element = `<li class="box fizz - ${i}"> fizz </li>`;
+    listElement.insertAdjacentHTML("beforeend", element);
   }
 }
